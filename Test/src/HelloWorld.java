97f2fa97589df11d229421f2ad1285145bdbdd39
Hisham Najem <hnaje080@uottawa.ca>
2016-08-29 15:23:48 -0400

    final commit

--- Test/src/HelloWorld.java
+++ Test/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("HELLO WORLD! -- will be commit 1");
 		System.out.println("THIS IS FROM MY BRANCH");
+		System.out.println("TO PULL FROM LOCAL MASTER");
 	}
 }
